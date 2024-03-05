dd0055ae51738c3e2d9fb7640b57aaeb5fdb671b
Dmitry Morozov <morozov124@gmail.com>
2024-03-05 14:11:57 +0300

    * Add 9.1_5.py

---
+++ 9.1_5.py
@@ -0,0 +1,51 @@
+import re
+from time import sleep
+
+from environs import Env
+from selenium import webdriver
+from selenium.webdriver import ActionChains
+from selenium.webdriver.support import expected_conditions as ec
+from selenium.webdriver.support.wait import WebDriverWait
+
+url = 'https://parsinger.ru/selenium/5.10/4/index.html'
+stepic = 'https://stepik.org/lesson/897512/step/11?unit=1066949'
+
+env = Env()
+env.read_env(recurse=False)
+
+options = webdriver.ChromeOptions()
+options.add_argument('--window-size=1920,1080')
+options.add_experimental_option("excludeSwitches", ['enable-automation'])
+
+with webdriver.Chrome(options=options) as driver:
+    wait = WebDriverWait(driver, 30)
+    actions = ActionChains(driver)
+    driver.get(url)
+
+    for ball in wait.until(
+            ec.presence_of_all_elements_located(('xpath', '//div[contains(@class, "ball_color")]'))
+    ):
+        color = re.findall(
+            '(\w+)_ball', ball.get_attribute('class')
+        )[0]
+        destination = wait.until(
+            ec.presence_of_element_located(('css selector', f'.{color}'))
+        )
+        actions.drag_and_drop(ball, destination)
+    actions.perform()
+
+    result = wait.until(
+        ec.visibility_of_element_located(('class name', 'message'))
+    ).text
+
+    driver.switch_to.new_window()
+    driver.get(stepic)
+    driver.add_cookie({'name': 'sessionid', 'value': env('STEPIK_SESSIONID')})
+    driver.refresh()
+    wait.until(
+        ec.visibility_of_element_located(('xpath', '//div[@class="attempt"]//textarea'))
+    ).send_keys(result)
+    wait.until(
+        ec.element_to_be_clickable(('xpath', '//button[@class="submit-submission"]'))
+    ).click()
+    sleep(5)
